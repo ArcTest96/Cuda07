1db5f500cb76eae7fd565bb392de65adb05124c1
Edgar Felipe <loquendero2010@live.com.mx>
2025-04-21 16:09:29 -0600

    Nuevos mensajes para sincronizar

--- Cuda07/Tri.cs
+++ Cuda07/Tri.cs
@@ -33,7 +33,7 @@ namespace Cuda07
             }
             else if (xD == 0)
             {
-                MessageBox.Show("PHP - Weeb xd");
+                MessageBox.Show("PHP - Weeb");
                 xD = 1;
             }
             
