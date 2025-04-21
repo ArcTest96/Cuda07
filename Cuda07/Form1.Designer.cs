5b0c046ed7330d8ad5af33e2ca9561df78e4c525
Edgar Felipe <loquendero2010@live.com.mx>
2025-04-21 16:01:09 -0600

    Check cambio mensajes

--- Cuda07/Form1.Designer.cs
+++ Cuda07/Form1.Designer.cs
@@ -71,7 +71,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(125, 187);
+            this.button2.Location = new System.Drawing.Point(128, 205);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
