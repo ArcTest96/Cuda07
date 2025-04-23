9d4b0e1edc3e79ede193f83e0ec7de666562d8f1
Edgar Felipe Hernandez Garcia <loquendero2010@live.com.mx>
2025-04-23 16:48:01 -0600

    Lado de boton

--- Cuda07/Form1.Designer.cs
+++ Cuda07/Form1.Designer.cs
@@ -45,7 +45,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(125, 158);
+            this.button1.Location = new System.Drawing.Point(338, 120);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
