8720d819987b212f8e8f87cc84d959fd731eb1aa
Edgar Felipe Hernandez Garcia <loquendero2010@live.com.mx>
2025-04-23 16:48:33 -0600

    Lo cambie de lado yo al izquierdo xd

--- Cuda07/Form1.Designer.cs
+++ Cuda07/Form1.Designer.cs
@@ -45,7 +45,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(125, 158);
+            this.button1.Location = new System.Drawing.Point(12, 120);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
